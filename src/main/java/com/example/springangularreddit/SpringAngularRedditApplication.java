2dce721e438ae41f078f81a1bc49c71d85fc8f9a
larinpaul <www.larinpavel@mail.ru>
2023-12-10 22:30:16 +0500

    Added the security folder and @EnableAsync and @Import(OpenAPICOnfiguration.class) in SpringAngularRedditApplication.java

--- src/main/java/com/example/springangularreddit/SpringAngularRedditApplication.java
+++ src/main/java/com/example/springangularreddit/SpringAngularRedditApplication.java
@@ -1,9 +1,14 @@
 package com.example.springangularreddit;
 
+import com.example.springangularreddit.config.OpenAPIConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
+@Import(OpenAPIConfiguration.class)
 public class SpringAngularRedditApplication {
 
 	public static void main(String[] args) {
