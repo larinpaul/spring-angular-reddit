d38754fbecc89b5f70dfc658d70f0b78a93e9ce1
larinpaul <www.larinpavel@mail.ru>
2023-12-15 17:35:40 +0500

    Added the mapper folder, I CommentMapper, C PostMapper, I SubredditMapper

---
+++ src/main/java/com/example/springangularreddit/mapper/SubredditMapper.java
@@ -0,0 +1,26 @@
+package com.example.springangularreddit.mapper;
+
+import com.example.springangularreddit.dto.SubredditDto;
+import com.example.springangularreddit.model.Post;
+import com.example.springangularreddit.model.Subreddit;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface SubredditMapper {
+
+    @Mapping(target = "numberOfPost", expression = "java(mapPost(subreddit.getPost()))")
+    SubredditDto mapSubredditToDto(Subreddit subreddit);
+
+    default Integer mapPosts(List<Post> numberOfPosts) {
+        return numberOfPosts.size();
+    }
+
+    @InheritInverseConfiguration
+    @Mapping(target = "posts", ignore = true)
+    Subreddit mapDtoToSubreddit(SubredditDto subredditDto);
+
+}
