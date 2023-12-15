4efe943b83d784f73f7c171480db572c8610dead
larinpaul <www.larinpavel@mail.ru>
2023-12-15 18:21:23 +0500

    Created the VoteDto in service

--- src/main/java/com/example/springangularreddit/dto/VoteDto.java
+++ src/main/java/com/example/springangularreddit/dto/VoteDto.java
@@ -1,4 +1,14 @@
 package com.example.springangularreddit.dto;
 
+import com.example.springangularreddit.model.VoteType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class VoteDto {
+    private VoteType voteType;
+    private Long postId;
 }
