854795d31ed56aeed4b3705030087470d2130743
larinpaul <www.larinpavel@mail.ru>
2024-03-10 12:22:41 +0500

    @EnableWebSecurity SecurityConfig, but extends WebSecurityConfigurerAdapter doesn't seem to exist, and also the method .antMatchers("/api/auth/**") doesn't seem to exist as well

--- src/main/java/com/example/springangularreddit/config/SecurityConfig.java
+++ src/main/java/com/example/springangularreddit/config/SecurityConfig.java
@@ -2,7 +2,8 @@
 //
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 //import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//// Doesn't seem to import anything. Maybe I got to change something with Maven or maybe this class is Obsolete
+////import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 //
 //@EnableWebSecurity
 //public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -10,13 +11,11 @@
 //    @Override
 //    public void configure(HttpSecurity httpSecurity) throws Exception {
 //        httpSecurity.csrf().disable()
-//            .authorizeRequests()
-//                .antMatchers("/api/auth/**")
+//                .authorizeRequests() // this method is obsolete?
+//                .antMatchers("/api/auth/**") // this method doesn't even seem to exist
 //                .permitAll()
 //                .anyRequest()
 //                .authenticated();
-//
 //    }
 //
 //}
-//
