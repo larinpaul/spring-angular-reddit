71917124dbcb56c89cf5b0741a515d032568c3dc
larinpaul <www.larinpavel@mail.ru>
2024-03-12 11:39:32 +0500

    Created MailService.java

--- src/main/java/com/example/springangularreddit/service/MailService.java
+++ src/main/java/com/example/springangularreddit/service/MailService.java
@@ -5,10 +5,8 @@ import com.example.springangularreddit.model.NotificationEmail;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.MailException;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,25 +14,63 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class MailService {
 
-    private final JavaMailSender mailSender;
-    private final MailContentBuilder mailContentBuilder;
-
-    @Async
-    void sendMail(NotificationEmail notificationEmail) {
+    public void sendMail(NotificationEmail notificationEmail) {
         MimeMessagePreparator messagePreparator = mimeMessage -> {
             MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
             messageHelper.setFrom("springreddit@email.com");
             messageHelper.setTo(notificationEmail.getRecipient());
             messageHelper.setSubject(notificationEmail.getSubject());
-            messageHelper.setText(notificationEmail.getBody());
+            messageHelper.setText(mailContentBuilder.build(notificationEmail.getBody()));
         };
         try {
             mailSender.send(messagePreparator);
             log.info("Activation email sent!!");
         } catch (MailException e) {
-            log.error("Exception occurred when sending mail", e);
-            throw new SpringRedditException("Exception occurred when sending mail to " + notificationEmail.getRecipient(), e);
+            throw new SpringRedditException("Exception occurred when sending main to " + notificationEmail.getRecipient());
         }
     }
 
 }
+
+
+
+//package com.example.springangularreddit.service;
+//
+//import com.example.springangularreddit.exceptions.SpringRedditException;
+//import com.example.springangularreddit.model.NotificationEmail;
+//import lombok.AllArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.mail.MailException;
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.mail.javamail.MimeMessageHelper;
+//import org.springframework.mail.javamail.MimeMessagePreparator;
+//import org.springframework.scheduling.annotation.Async;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@AllArgsConstructor
+//@Slf4j
+//public class MailService {
+//
+//    private final JavaMailSender mailSender;
+//    private final MailContentBuilder mailContentBuilder;
+//
+//    @Async
+//    void sendMail(NotificationEmail notificationEmail) {
+//        MimeMessagePreparator messagePreparator = mimeMessage -> {
+//            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+//            messageHelper.setFrom("springreddit@email.com");
+//            messageHelper.setTo(notificationEmail.getRecipient());
+//            messageHelper.setSubject(notificationEmail.getSubject());
+//            messageHelper.setText(notificationEmail.getBody());
+//        };
+//        try {
+//            mailSender.send(messagePreparator);
+//            log.info("Activation email sent!!");
+//        } catch (MailException e) {
+//            log.error("Exception occurred when sending mail", e);
+//            throw new SpringRedditException("Exception occurred when sending mail to " + notificationEmail.getRecipient(), e);
+//        }
+//    }
+//
+//}
