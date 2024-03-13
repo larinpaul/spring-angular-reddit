9cb13db3b3493c94e0be1bcacabd5b063149cdb8
larinpaul <www.larinpavel@mail.ru>
2024-03-13 23:06:26 +0500

    To make an application asynchronous, just add @EnableAsync! :)

--- src/main/java/com/example/springangularreddit/SpringAngularRedditApplication.java
+++ src/main/java/com/example/springangularreddit/SpringAngularRedditApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+
 @SpringBootApplication
 @EnableAsync
 @Import(OpenAPIConfiguration.class)
