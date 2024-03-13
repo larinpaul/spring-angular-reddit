7920fd8554a2825a933e08b848aa52e27cfe58e3
larinpaul <www.larinpavel@mail.ru>
2024-03-13 23:08:21 +0500

    To enable the asynchronicity of the application, just add @EnableAsync! :) AND don't forget to add @Async on the method that should be asynchronous

--- src/main/java/com/example/springangularreddit/service/MailService.java
+++ src/main/java/com/example/springangularreddit/service/MailService.java
@@ -5,8 +5,10 @@ import com.example.springangularreddit.model.NotificationEmail;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,6 +16,10 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class MailService {
 
+    private final JavaMailSender mailSender;
+    private final MailContentBuilder mailContentBuilder;
+
+    @Async
     public void sendMail(NotificationEmail notificationEmail) {
         MimeMessagePreparator messagePreparator = mimeMessage -> {
             MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
