33b58e7db9b2c3b35b2e4299681d10ab9fce54fa
larinpaul <www.larinpavel@mail.ru>
2024-03-14 10:17:13 +0500

    Added @Autowired public void configuredGlobal(AutheticationManagerBuilder authenticationManagerBuilder) {}

--- src/main/java/com/example/springangularreddit/config/SecurityConfig.java
+++ src/main/java/com/example/springangularreddit/config/SecurityConfig.java
@@ -1,29 +1,42 @@
-//package com.example.springangularreddit.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//// Doesn't seem to import anything. Maybe I got to change something with Maven or maybe this class is Obsolete
-////import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-//
-//@EnableWebSecurity
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    public void configure(HttpSecurity httpSecurity) throws Exception {
-//        httpSecurity.csrf().disable()
-//                .authorizeRequests() // this method is obsolete?
-//                .antMatchers("/api/auth/**") // this method doesn't even seem to exist
-//                .permitAll()
-//                .anyRequest()
-//                .authenticated();
-//    }
-//
-//    @Bean
-//    PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//}
+package com.example.springangularreddit.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+// Doesn't seem to import anything. Maybe I got to change something with Maven or maybe this class is Obsolete
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .authorizeRequests() // this method is obsolete?
+                .antMatchers("/api/auth/**") // this method doesn't even seem to exist
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+
+    @Autowired // We inject this method using a method injection
+    public void configuredGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) {
+        authenticationManagerBuilder.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
