7a3a8fa713e4fc40e896b7a27edf84ed6525eefc
larinpaul <www.larinpavel@mail.ru>
2024-03-17 21:07:23 +0500

    Added JwtProvider with a field private KeyStore keyStore; and methods @PostConstruct public void init() {} public String generateToken(Authentication authentication) {} private PrivateKey getPrivateKey() {}

--- src/main/java/com/example/springangularreddit/security/JwtProvider.java
+++ src/main/java/com/example/springangularreddit/security/JwtProvider.java
@@ -1,43 +1,96 @@
 package com.example.springangularreddit.security;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import com.example.springangularreddit.exceptions.SpringRedditException;
+import io.jsonwebtoken.Jwts;
+import jakarta.annotation.PostConstruct;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.oauth2.jwt.JwtClaimsSet;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.User;
 
-import java.time.Instant;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.*;
+import java.security.cert.CertificateException;
 
 @Service
-@RequiredArgsConstructor
 public class JwtProvider {
 
-    private final JwtEncoder jwtEncoder;
-    @Value("${jwt.expiration.time}")
-    private Long jwtExpirationInMillis;
+    private KeyStore keyStore;
+
+    @PostConstruct
+    public void init() {
+        try {
+            keyStore = KeyStore.getInstance("JKS");
+            InputStream resourceAsStream = getClass().getResourceAsStream("/springblog.jks");
+            keyStore.load(resourceAsStream, "secret".toCharArray());
+        } catch (KeyStoreException | CertificateException | NoSuchAlgorithmException | IOException e) {
+            throw new SpringRedditException("Exception occurred while loading keystore");
+        }
+    }
 
     public String generateToken(Authentication authentication) {
-        User principal = (User) authentication.getPrincipal();
-        return generateTokenWithUserName(principal.getUsername());
+        // This is the one I wrote:
+        org.springframework.security.core.userdetails.User principalLong = (org.springframework.security.core.userdetails.User) authentication.getPrincipal();
+        // This is the one he has (probably imported User separately):
+        org.springframework.security.core.userdetails.User principal = (User) authentication.getPrincipal();
+        return Jwts.builder()
+                .setSubject(principal.getUsername())
+                .signWith(getPrivateKey())
+                .compact();
     }
 
-    public String generateTokenWithUserName(String username) {
-        JwtClaimsSet claims = JwtClaimsSet.builder()
-                .issuer("self")
-                .issuedAt(Instant.now())
-                .expiresAt(Instant.now().plusMillis(jwtExpirationInMillis))
-                .subject(username)
-                .claim("scope", "ROLE_USER")
-                .build();
-
-        return this.jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    private PrivateKey getPrivateKey() {
+        try {
+            return (PrivateKey) keyStore.getKey("springblog", "secret".toCharArray());
+        } catch (KeyStoreException | NoSuchAlgorithmException | UnrecoverableKeyException e) {
+            throw new SpringRedditException("Exception occurred while retrieving public key from keys");
+        }
     }
 
-    public Long getJwtExpirationInMillis() {
-        return jwtExpirationInMillis;
-    }
 
 }
+
+
+//package com.example.springangularreddit.security;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+//import org.springframework.security.oauth2.jwt.JwtEncoder;
+//import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+//import org.springframework.stereotype.Service;
+//
+//import java.time.Instant;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class JwtProvider {
+//
+//    private final JwtEncoder jwtEncoder;
+//    @Value("${jwt.expiration.time}")
+//    private Long jwtExpirationInMillis;
+//
+//    public String generateToken(Authentication authentication) {
+//        User principal = (User) authentication.getPrincipal();
+//        return generateTokenWithUserName(principal.getUsername());
+//    }
+//
+//    public String generateTokenWithUserName(String username) {
+//        JwtClaimsSet claims = JwtClaimsSet.builder()
+//                .issuer("self")
+//                .issuedAt(Instant.now())
+//                .expiresAt(Instant.now().plusMillis(jwtExpirationInMillis))
+//                .subject(username)
+//                .claim("scope", "ROLE_USER")
+//                .build();
+//
+//        return this.jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+//    }
+//
+//    public Long getJwtExpirationInMillis() {
+//        return jwtExpirationInMillis;
+//    }
+//
+//}
