caf95ad76c0a872732e57e9b0309e3595fa3b792
larinpaul <www.larinpavel@mail.ru>
2024-03-18 11:19:05 +0500

    SubredditController.java @RestController @RequestMapping("/api/subreddit") @AllArgsConstructor @Slf4j public class SubredditController { @PostMapping public void createSubreddit(@RequestBody SubredditDto subredditDto) }  and  SubredditDto.java package com.example.springangularreddit.dto; import lombok.AllArgsConstructor; import lombok.Data; import lombok.NoArgsConstructor; @Data @AllArgsConstructor @NoArgstConstructor public class SubredditDto { private Long id; private String name; private String description; private Integer numberOfPosts; }

--- src/main/java/com/example/springangularreddit/controller/SubredditController.java
+++ src/main/java/com/example/springangularreddit/controller/SubredditController.java
@@ -1,14 +1,12 @@
 package com.example.springangularreddit.controller;
 
 import com.example.springangularreddit.dto.SubredditDto;
-import com.example.springangularreddit.service.SubredditService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/subreddit")
@@ -16,26 +14,54 @@ import java.util.List;
 @Slf4j
 public class SubredditController {
 
-    private final SubredditService subredditService;
-
     @PostMapping
-    public ResponseEntity<SubredditDto> createSubreddit(@RequestBody SubredditDto subredditDto) {
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(subredditService.save(subredditDto));
-    }
+    public void createSubreddit(@RequestBody SubredditDto subredditDto) {
 
-    @GetMapping
-    public ResponseEntity<List<SubredditDto>> getAllSubreddits() {
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(subredditService.getAll());
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<SubredditDto> getSubreddit(@PathVariable Long id) {
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(subredditService.getSubreddit(id));
     }
 
 }
+
+
+
+
+//package com.example.springangularreddit.controller;
+//
+//import com.example.springangularreddit.dto.SubredditDto;
+//import com.example.springangularreddit.service.SubredditService;
+//import lombok.AllArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/api/subreddit")
+//@AllArgsConstructor
+//@Slf4j
+//public class SubredditController {
+//
+//    private final SubredditService subredditService;
+//
+//    @PostMapping
+//    public ResponseEntity<SubredditDto> createSubreddit(@RequestBody SubredditDto subredditDto) {
+//        return ResponseEntity.status(HttpStatus.CREATED)
+//                .body(subredditService.save(subredditDto));
+//    }
+//
+//    @GetMapping
+//    public ResponseEntity<List<SubredditDto>> getAllSubreddits() {
+//        return ResponseEntity
+//                .status(HttpStatus.OK)
+//                .body(subredditService.getAll());
+//    }
+//
+//    @GetMapping("/{id}")
+//    public ResponseEntity<SubredditDto> getSubreddit(@PathVariable Long id) {
+//        return ResponseEntity
+//                .status(HttpStatus.OK)
+//                .body(subredditService.getSubreddit(id));
+//    }
+//
+//}
