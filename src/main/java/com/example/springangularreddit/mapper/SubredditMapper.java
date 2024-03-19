8d25d3bede1a819c3e80b3f5f3a288cf9f19c778
larinpaul <www.larinpavel@mail.ru>
2024-03-19 12:34:38 +0500

    Starting to work with MapStruct in SubredditMapper.java

--- src/main/java/com/example/springangularreddit/mapper/SubredditMapper.java
+++ src/main/java/com/example/springangularreddit/mapper/SubredditMapper.java
@@ -3,7 +3,6 @@ package com.example.springangularreddit.mapper;
 import com.example.springangularreddit.dto.SubredditDto;
 import com.example.springangularreddit.model.Post;
 import com.example.springangularreddit.model.Subreddit;
-import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -12,15 +11,44 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface SubredditMapper {
 
-    @Mapping(target = "numberOfPost", expression = "java(mapPost(subreddit.getPost()))")
+    // compile the code in IntelliJIDEA by pressing Ctr+Shift+F9
+    // and it is supposed to create SubredditMapperImpl, I guess...
+
+    @Mapping(target = "numberOfPosts", expression = "java(mapPosts(subreddit.getPosts()))")
     SubredditDto mapSubredditToDto(Subreddit subreddit);
 
     default Integer mapPosts(List<Post> numberOfPosts) {
         return numberOfPosts.size();
     }
 
-    @InheritInverseConfiguration
-    @Mapping(target = "posts", ignore = true)
-    Subreddit mapDtoToSubreddit(SubredditDto subredditDto);
+
 
 }
+
+
+//package com.example.springangularreddit.mapper;
+//
+//import com.example.springangularreddit.dto.SubredditDto;
+//import com.example.springangularreddit.model.Post;
+//import com.example.springangularreddit.model.Subreddit;
+//import org.mapstruct.InheritInverseConfiguration;
+//import org.mapstruct.Mapper;
+//import org.mapstruct.Mapping;
+//
+//import java.util.List;
+//
+//@Mapper(componentModel = "spring")
+//public interface SubredditMapper {
+//
+//    @Mapping(target = "numberOfPost", expression = "java(mapPost(subreddit.getPost()))")
+//    SubredditDto mapSubredditToDto(Subreddit subreddit);
+//
+//    default Integer mapPosts(List<Post> numberOfPosts) {
+//        return numberOfPosts.size();
+//    }
+//
+//    @InheritInverseConfiguration
+//    @Mapping(target = "posts", ignore = true)
+//    Subreddit mapDtoToSubreddit(SubredditDto subredditDto);
+//
+//}
