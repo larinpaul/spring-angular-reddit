3c2396359cbfcc3f79c0f30cdb851f08b912597e
larinpaul <www.larinpavel@mail.ru>
2024-03-27 09:30:33 +0500

    Changed PostMapper from an interface to an abstract class. Also added TimeAgo (a Kotlin library)

--- src/main/java/com/example/springangularreddit/mapper/PostMapperImpl.java
+++ src/main/java/com/example/springangularreddit/mapper/PostMapperImpl.java
@@ -4,4 +4,6 @@ public class PostMapperImpl {
 
     // TODO Here @Mapping and @Mapper are supposed to create the mapper classes
 
+    // TODO Also it is supposed to have been changed, since we've changed PostMapper from being an interface to being an abstract class
+
 }
